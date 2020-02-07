8e1376e2e0c2605a61942fd50e8016bcd63f9b38
Luca Scaringella <lucalstyle@gmail.com>
2020-02-07 02:59:50 +0100

    Use the pascal case of the name for the app name in the app config.

--- templates/server/src/configs/main.config.ts
+++ templates/server/src/configs/main.config.ts
@@ -3,7 +3,7 @@ import {Config} from 'zation-server';
 module.exports = Config.mainConfig(
     {
         port: {{port}},
-        appName: '{{name}}',
+        appName: '{{name}/pc}',
         usePanel: true,
         panelUser: {username: 'admin', password: 'admin'},
     });
