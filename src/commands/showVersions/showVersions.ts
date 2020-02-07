ede3d998ab228c97a6ed537419fbf144257c170c
Luca Scaringella <lucalstyle@gmail.com>
2020-02-07 02:29:09 +0100

    Add support for zation bundle versions.

--- src/commands/showVersions/showVersions.ts
+++ src/commands/showVersions/showVersions.ts
@@ -1,7 +1,14 @@
 import * as vscode from 'vscode';
-import { zationServerVersion, zationClientVersion, zationAssuredVersion, zationClusterStateVersion, zationClusterBrokerVersion } from '../../versions';
+import {
+    zationServerVersion,
+    zationClientVersion,
+    zationAssuredVersion,
+    zationClusterStateVersion,
+    zationClusterBrokerVersion,
+    zationBundleVersion
+} from '../../versions';
 
 export function showVersions() {
     vscode.window.showInformationMessage(
-   `Zation Server Version: ${zationServerVersion}, Zation Client Version: ${zationClientVersion}, Zation Assured version: ${zationAssuredVersion}, Zation Cluster State version: ${zationClusterStateVersion}, Zation Cluster Broker version: ${zationClusterBrokerVersion}`);
+   `Zation Bundle Version: ${zationBundleVersion}, Zation Server Version: ${zationServerVersion}, Zation Client Version: ${zationClientVersion}, Zation Assured version: ${zationAssuredVersion}, Zation Cluster State version: ${zationClusterStateVersion}, Zation Cluster Broker version: ${zationClusterBrokerVersion}`);
 }
--- src/versions.ts
+++ src/versions.ts
@@ -1,3 +1,4 @@
+export const zationBundleVersion = '0.0.0';
 export const zationServerVersion = '1.4.2';
 export const zationClientVersion = '1.0.1';
 export const zationAssuredVersion = '0.3.5';
@@ -7,6 +8,7 @@ export const zationClusterStateVersion = '6.0.6';
 export const zationClusterBrokerVersion = '6.0.7';
 
 export const versions = {
+    zationBundleVersion,
     zationServerVersion,
     zationClientVersion,
     zationAssuredVersion,
