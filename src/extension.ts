d8a6a5709d0f247b03b1958d3d56fbf95f6c1b07
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-06-26 23:50:51 +0200

    Remove clone cluster component command.

--- src/extension.ts
+++ src/extension.ts
@@ -6,16 +6,12 @@ Copyright(c) Ing. Luca Gian Scaringella
 
 import * as vscode from 'vscode';
 import { createNewProject } from './commands/newProject/createNewProject';
-import { cloneClusterComponent } from './commands/cloneClusterComponent/cloneClusterComponent';
 import { showVersions } from './commands/showVersions/showVersions';
 import {callCommandSafe} from "./shared/commandCaller";
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('zation.NewProject',
 		() => callCommandSafe(createNewProject)));
-
-	context.subscriptions.push(vscode.commands.registerCommand('zation.CloneClusterComponent',
-		() => callCommandSafe(cloneClusterComponent)));
 
 	context.subscriptions.push(vscode.commands.registerCommand('zation.ShowVersions',
 		() => callCommandSafe(showVersions)));
